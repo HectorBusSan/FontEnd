8f91ce49b0d77a24098917bc63cb54e213a21e91
HectorBusSan <99218752+HectorBusSan@users.noreply.github.com>
2022-03-31 08:30:26 -0600

    Add files via upload

---
+++ Vue/public/src/components/MenuP.vue
@@ -0,0 +1,18 @@
+<template>
+  <li class="item__pastel">{{textP}} <p>{{price}}</p></li>
+</template>
+
+<script>
+export default {
+  name: 'MenuP',
+  props: {
+    textP: String,
+    price: String
+  }
+}
+</script>
+<style scoped>
+    .item__pastel{
+        color: #fff;
+    }
+</style>
